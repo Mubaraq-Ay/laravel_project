18829d690063dafbc2adf2fe50d48b498b589bb9
Mubaraq-Ay <ayanlekemubaraq@gmail.com>
2024-10-24 13:24:37 -0700

    modified

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
 
 
 
@@ -12,3 +13,7 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
  
 Route::resource('jobs', JobController::class);
+
+//Auth
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
